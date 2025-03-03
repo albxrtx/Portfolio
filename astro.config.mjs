ac88ede3ec70cc70279f669902d436f140843886
albxrtx <albertomariannechita@gmail.com>
2025-03-03 19:05:45 +0100

    workflows

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,8 @@
-import { defineConfig } from 'astro/config';
-import tailwind from '@astrojs/tailwind';
+import { defineConfig } from "astro/config";
+import tailwind from "@astrojs/tailwind";
 
 export default defineConfig({
+  site: "https://albxrtx.github.io",
+  base: "Portfolio",
   integrations: [tailwind()],
 });
